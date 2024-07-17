42751a53c142473042d1035441759738a0aff5ac
Americo2023 <americo.perez@icloud.com>
2024-07-17 23:48:41 +0200

    End section 16

---
+++ client/src/app/admin/photo-manegement/photo-manegement.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-manegement',
+  standalone: true,
+  imports: [],
+  templateUrl: './photo-manegement.component.html',
+  styleUrl: './photo-manegement.component.css'
+})
+export class PhotoManegementComponent {
+
+}
